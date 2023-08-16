0eb7e7ee0485b5b005c1639522f9064b9a04f415
DS23666X <diasgsputra@gmail.com>
2023-08-16 08:59:34 +0700

    allow cors

--- src/main/java/com/example/employee/controller/EmployeeController.java
+++ src/main/java/com/example/employee/controller/EmployeeController.java
@@ -7,7 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+
 @RestController
+@CrossOrigin
 @RequestMapping("/api/employees")
 public class EmployeeController {
   private final EmployeeService employeeService;
